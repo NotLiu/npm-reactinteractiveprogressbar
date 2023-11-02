78303e9f8450f5d3e8d129326b9de6c206db53f9
NotLiu <aliu@lediur.com>
2023-11-01 21:27:58 -0400

    refactor of readme, huge feature update, rewrite component

--- README.md
+++ README.md
@@ -1,11 +1,85 @@
-# npm-react-typescript-template
+# React-Interactive-Progress-Bar
 
-A template for publishing a React + TypeScript package to npm
+## Overview
 
-## How to use
+### React Interactive Progress Bar
 
-Fork this repo, clone it to your local computer, and edit the `package.json` along with every other required file to match your project.
-Write the code for your package in TypeScript and Sass, compile it, and publish it to [npm](https://npmjs.com).
+A simple React component for creating a simple customizable interactive progress bar.
+Use cases:
+
+- Keep track of progress for different elements in segments
+- Interactive widget for selecting values in customizable segments
+- Health Bar! Progress Bar! Resource Bar!
+
+## How to Use
+
+#### install npm package
+
+`npm install react-interactive-progress-bar`
+
+#### Put it into your project
+
+#### No props passed
+
+`<ProgressBar />`:\
+![Alt text](image.png)
+
+#### {primaryColor, secondaryColor, tertiaryColor} props passed (strings)
+
+- primaryColor for fill color
+- secondaryColor for negative fill color
+- tertiaryColor for border, notches, outline, edit arrow colors
+
+      <ProgressBar primaryColor="#cb362b" secondaryColor="#2e7858" tertiaryColor="#112142" />
+
+  ![Alt text](image-1.png)
+
+#### {segments} props passed to define number of sections
+
+    <ProgressBar primaryColor="#cb362b" secondaryColor="#2e7858" tertiaryColor="#112142" segments={10} />
+
+![Alt text](image-2.png)
+
+#### {edit} props passed to toggle user interactivity / disable
+
+    <ProgressBar primaryColor="#cb362b" secondaryColor="#2e7858" tertiaryColor="#112142" segments={10} edit={false} />
+
+![Alt text](image-3.png)
+
+#### {fill, width} props passed to predefine # segments filled and width of bar
+
+    <ProgressBar primaryColor="#cb362b" secondaryColor="#2e7858" tertiaryColor="#112142" segments={10} edit={true} fill={fillState} width={"50rem"} />
+
+![Alt text](image-4.png)
+
+#### {stateHandler} props passed to custom define a state function with the current fill value passed as an argument
+
+![Alt text](image-5.png)
+
+# Props
+
+All props are optional
+
+    segments?: number
+    fill?: number
+    primaryColor?: string
+    secondaryColor?: string
+    tertiaryColor?: string
+    edit?: boolean
+    width?: string
+
+- primaryColor - fill color
+- secondaryColor - negative fill color
+- tertiaryColor - border, notch, edit arrow color
+- segments - number of segments on progress bar
+- edit - toggle interactivity of progress bar
+- fill - initial fill value of progress bar
+- width - width of progress bar
+- stateHandler - function that accepts current fill value as argument
+
+## How to Develop
+
+### Code courtesy of npm react package template
 
 To compile your code once, run
 
@@ -31,4 +105,17 @@ Then publish your package by running
 
 - `npm publish`
 
-### Happy Building ♡
+## Release Notes
+
+#### `v2.0.0` Second Release:
+
+- Remove unused code, clean up code, standardize code and styling
+- Fix independent tracker functionality
+- Make design responsive and redesign component!
+- Fix custom color importing
+- Fix props and width styling
+
+#### `v1.0.0` Initial Release:
+
+- Code taken and refactored to run independent from the Commission Tracker project
+- First release to NPM!
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,6 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import styled, { keyframes } from 'styled-components'
-
-export declare interface AppProps {
-  segments: number
-  fill: number
-  progressEmpty: boolean
-  primarycolor: string
-  accesscolor: string
-  edit: boolean
-}
+import { BsCaretLeftFill, BsCaretRightFill } from 'react-icons/bs'
 
 const moveProgress = (progressPercentage: string, oldPercentage: string) => {
   return keyframes`
@@ -22,30 +14,43 @@ const moveProgress = (progressPercentage: string, oldPercentage: string) => {
 }
 
 const RoundedBarCSS = styled.div<{
-  accesscolor: string
+  secondaryColor: string
   primaryColor: string
+  barWidth: string
+  tertiaryColor: string
 }>`
   position: absolute;
   border-width: 5px;
-  border-color: ${({ accesscolor }) => accesscolor || 'black'};
+  border-color: ${({ secondaryColor, tertiaryColor }) =>
+    tertiaryColor !== undefined
+      ? tertiaryColor || 'black'
+      : secondaryColor || 'black'};
   border-radius: 5em;
   border-style: solid;
   height: 1em;
   z-index: 10;
-  width: inherit;
+  width: ${({ barWidth }) => barWidth || '50rem'};
 `
 
 const BarInsideCSS = styled.div<{
-  accesscolor: string
+  secondaryColor: string
   primaryColor: string
   progressEmpty: boolean
+  tertiaryColor: string
 }>`
   position: absolute;
+  top: -2px;
+  left: -2px;
   border-radius: 10em;
-  background-color: ${({ progressEmpty, primaryColor, accesscolor }) =>
-    progressEmpty ? primaryColor || 'white' : accesscolor || 'black'};
+  border-width: 3.5px;
+  background-color: ${({ progressEmpty, primaryColor, secondaryColor }) =>
+    progressEmpty ? primaryColor || 'white' : secondaryColor || 'black'};
   border-style: solid;
-  width: 99.5%;
+  border-color: ${({ secondaryColor, tertiaryColor }) =>
+    tertiaryColor !== undefined
+      ? tertiaryColor || 'black'
+      : secondaryColor || 'black'};
+  width: calc(100% - 2px);
   height: 80%;
   z-index: 1;
 `
@@ -54,18 +59,20 @@ const BarInsideNegCSS = styled.div<{
   progressPercentage: string
   primaryColor: string
   oldPercentage: string
-  accesscolor: string
+  secondaryColor: string
+  tertiaryColor: string
 }>`
   position: absolute;
   background-color: ${({ primaryColor }) => primaryColor || 'white'};
+
   border-top-right-radius: 5em;
   border-bottom-right-radius: 5em;
   border-bottom-left-radius: ${({ progressPercentage }) =>
-    (progressPercentage == '0%' && '5em') || 0};
+    (progressPercentage === '0%' && '5em') || 0};
   border-top-left-radius: ${({ progressPercentage }) =>
-    (progressPercentage == '0%' && '5em') || 0};
-  top: -5%;
-  height: 110%;
+    (progressPercentage === '0%' && '5em') || 0};
+  /* top: -5%; */
+  height: 100%;
   z-index: 2;
   right: 0px;
   width: ${({ progressPercentage }) =>
@@ -78,19 +85,23 @@ const BarInsideNegCSS = styled.div<{
 
 const ProgressNotch = styled.div<{
   progressPercentage: string
-  accesscolor: string
+  secondaryColor: string
   primaryColor: string
+  tertiaryColor: string
 }>`
   position: absolute;
   left: ${({ progressPercentage }) => progressPercentage || '0%'};
-  top: -100%;
-  background-color: ${({ accesscolor }) => accesscolor || 'black'};
+  top: -50%;
+  background-color: ${({ secondaryColor, tertiaryColor }) =>
+    tertiaryColor !== undefined
+      ? tertiaryColor || 'black'
+      : secondaryColor || 'black'};
   /* border-style: solid; */
   border-radius: 2em;
   border-color: ${({ primaryColor }) => primaryColor || 'white'};
   border-width: 3px;
   width: 5px;
-  height: 300%;
+  height: 200%;
   z-index: 5;
 `
 
@@ -109,42 +120,65 @@ const ProgressUlCSS = styled.ul`
   padding: 0 0;
   display: inline-flex;
   flex-direction: row;
-  width: inherit;
+  width: 100%;
 `
 
 const ProgressLiCSS = styled.li`
-  width: inherit;
+  width: 100%;
   padding: 0 0;
   list-style: none;
   /* padding-left: 2em; */
   z-index: 8;
 `
 
-// export const ArrowsCSS = styled.div`
-//   position: absolute;
-//   margin-left: 20.5em;
-//   padding-top: 0.25em;
-//   width: 2em;
-//   color: ${(props) => props.accesscolor || "black"};
-//   z-index: 1;
-// `;
-export default function ProgressBar(props: AppProps) {
-  const [percentage, setPercentage] = React.useState(
-    props.fill * (100 / props.segments) + '%'
+export const ArrowsCSS = styled.div<{
+  secondaryColor: string
+  tertiaryColor: string
+}>`
+  /* position: absolute; */
+  /* margin-left: 20.5em; */
+  padding-top: 0.25em;
+  width: 2em;
+  color: ${({ secondaryColor, tertiaryColor }) =>
+    tertiaryColor !== undefined
+      ? tertiaryColor || 'black'
+      : secondaryColor || 'black'};
+  z-index: 1;
+`
+
+interface props {
+  segments?: number
+  fill?: number
+  primaryColor?: string
+  secondaryColor?: string
+  tertiaryColor?: string
+  edit?: boolean
+  width?: string
+}
+
+export default function ProgressBar({
+  segments = 4,
+  fill = 1,
+  primaryColor = 'white',
+  secondaryColor = 'black',
+  tertiaryColor = 'black',
+  edit = true,
+  width = '20rem',
+}: props) {
+  const [percentage, setPercentage] = useState(fill * (100 / segments) + '%')
+  const [lastPercentage, setLastPercentage] = useState(
+    fill * (100 / segments) + '%'
   )
-  const [lastPercentage, setLastPercentage] = React.useState(
-    props.fill * (100 / props.segments) + '%'
-  )
-  const [progressEmpty, setProgressempty] = React.useState(props.progressEmpty)
-  const [lastSegment, setlastSegment] = React.useState(props.fill)
-  const [secondlastSegment, setSecondlastSegment] = React.useState(props.fill)
-  const numSegments = props.segments
+  const [progressEmpty, setProgressempty] = useState(false)
+  const [lastSegment, setlastSegment] = useState(fill)
+  const [secondlastSegment, setSecondlastSegment] = useState(fill)
+  const numSegments = segments
 
   function handleProgress(segment: number) {
     setlastSegment(segment)
     setSecondlastSegment(lastSegment)
     setLastPercentage(percentage)
-    if (segment == lastSegment && segment == 1 && progressEmpty == false) {
+    if (segment === lastSegment && segment === 1 && progressEmpty === false) {
       // setProgressempty(true);
     } else {
       setProgressempty(false)
@@ -152,10 +186,10 @@ export default function ProgressBar(props: AppProps) {
 
     //logic for making sure clicking progress bar functions properly, e.g. clicking lit section unlights it, after unlighting a section you can unlight the next lit section in one click, you can light and unlight sections repeatedly
     if (
-      (segment == lastSegment &&
-        progressEmpty == false &&
-        lastSegment != secondlastSegment) ||
-      segment * (100 / numSegments) + '%' == percentage
+      (segment === lastSegment &&
+        progressEmpty === false &&
+        lastSegment !== secondlastSegment) ||
+      segment * (100 / numSegments) + '%' === percentage
     ) {
       setPercentage((segment - 1) * (100 / numSegments) + '%')
     } else {
@@ -163,26 +197,26 @@ export default function ProgressBar(props: AppProps) {
     }
   }
 
-  // function handleArrows(change) {
-  //   setlastSegment(
-  //     (parseFloat(percentage.slice(0, -1)) + change) / (100 / numSegments)
-  //   );
-  //   setSecondlastSegment(lastSegment);
-  //   setLastPercentage(percentage);
-  //   if (
-  //     !(parseFloat(percentage.slice(0, -1)) + change > 100) &&
-  //     !(parseFloat(percentage.slice(0, -1)) + change < 0)
-  //   ) {
-  //     if (parseFloat(percentage.slice(0, -1)) + change + "%" == "0%") {
-  //       // setProgressempty(true);
-  //     } else {
-  //       setProgressempty(false);
-  //     }
-  //     setPercentage(parseFloat(percentage.slice(0, -1)) + change + "%");
-  //   } else {
-  //     console.log("ARROW CHANGE OUT OF BOUNDS");
-  //   }
-  // }
+  function handleArrows(change) {
+    setlastSegment(
+      (parseFloat(percentage.slice(0, -1)) + change) / (100 / numSegments)
+    )
+    setSecondlastSegment(lastSegment)
+    setLastPercentage(percentage)
+    if (
+      !(parseFloat(percentage.slice(0, -1)) + change > 100) &&
+      !(parseFloat(percentage.slice(0, -1)) + change < 0)
+    ) {
+      if (parseFloat(percentage.slice(0, -1)) + change + '%' === '0%') {
+        // setProgressempty(true);
+      } else {
+        setProgressempty(false)
+      }
+      setPercentage(parseFloat(percentage.slice(0, -1)) + change + '%')
+    } else {
+      console.log('ARROW CHANGE OUT OF BOUNDS')
+    }
+  }
 
   function generateNotches() {
     const notches: JSX.Element[] = []
@@ -190,8 +224,9 @@ export default function ProgressBar(props: AppProps) {
       notches.push(
         <ProgressNotch
           progressPercentage={(100 / numSegments) * i + '%'}
-          primaryColor={props.primarycolor}
-          accesscolor={props.accesscolor}
+          primaryColor={primaryColor}
+          secondaryColor={secondaryColor}
+          tertiaryColor={tertiaryColor}
           key={i}
         />
       )
@@ -220,45 +255,51 @@ export default function ProgressBar(props: AppProps) {
     <ProgressUlCSS>
       <ProgressLiCSS>
         <RoundedBarCSS
-          primaryColor={props.primarycolor}
-          accesscolor={props.accesscolor}
+          primaryColor={primaryColor}
+          secondaryColor={secondaryColor}
+          tertiaryColor={tertiaryColor}
+          barWidth={width}
         >
           {generateNotches()}
-          {props.edit && generateButtons()}
+          {edit && generateButtons()}
 
           <BarInsideCSS
             progressEmpty={progressEmpty}
-            primaryColor={props.primarycolor}
-            accesscolor={props.accesscolor}
+            primaryColor={primaryColor}
+            secondaryColor={secondaryColor}
+            tertiaryColor={tertiaryColor}
           >
             <BarInsideNegCSS
               progressPercentage={percentage}
               oldPercentage={lastPercentage}
-              primaryColor={props.primarycolor}
-              accesscolor={props.accesscolor}
+              primaryColor={primaryColor}
+              secondaryColor={secondaryColor}
+              tertiaryColor={tertiaryColor}
             ></BarInsideNegCSS>
           </BarInsideCSS>
         </RoundedBarCSS>
       </ProgressLiCSS>
-      {/* <ProgressLiCSS>
-          {props.edit && (
-            <ArrowsCSS
-              primaryColor={props.primaryColor}
-              accesscolor={props.accesscolor}
-            >
-              <BsFillCaretLeftFill
-                onClick={() => {
-                  handleArrows(-(100 / numSegments));
-                }}
-              />
-              <BsFillCaretRightFill
-                onClick={() => {
-                  handleArrows(100 / numSegments);
-                }}
-              />
-            </ArrowsCSS>
-          )}
-        </ProgressLiCSS> */}
+      <ProgressLiCSS
+        style={{ position: 'absolute', left: `calc(${width} + 2rem)` }}
+      >
+        {edit && (
+          <ArrowsCSS
+            secondaryColor={secondaryColor}
+            tertiaryColor={tertiaryColor}
+          >
+            <BsCaretLeftFill
+              onClick={() => {
+                handleArrows(-(100 / numSegments))
+              }}
+            />
+            <BsCaretRightFill
+              onClick={() => {
+                handleArrows(100 / numSegments)
+              }}
+            />
+          </ArrowsCSS>
+        )}
+      </ProgressLiCSS>
     </ProgressUlCSS>
   )
 }
